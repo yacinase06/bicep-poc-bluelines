10d0663586133218d4c3230464100c6792db4c37
Shaun Croucher <shauncroucher@gmail.com>
2021-08-21 15:30:34 +0100

    cse update code to work

--- scripts/cse.sh
+++ scripts/cse.sh
@@ -3,8 +3,6 @@
 # setup of tunnel reference - https://sysadmins.co.za/setup-a-site-to-site-ipsec-vpn-with-strongswan-on-ubuntu/   and  https://www.tecmint.com/setup-ipsec-vpn-with-strongswan-on-debian-ubuntu/
 sudo apt-get update
 sudo apt install strongswan --yes
-cat >> /etc/sysctl.conf << EOF
-echo net.ipv4.ip_forward = 1
-net.ipv4.conf.all.accept_redirects = 0
-net.ipv4.conf.all.send_redirects = 0
-EOF
+sudo sed -i '/'net.ipv4.conf.all.accept_redirects'/s/^#//g' /etc/sysctl.conf 
+sudo sed -i '/'net.ipv4.conf.all.send_redirects'/s/^#//g' /etc/sysctl.conf 
+sudo sed -i '/'net.ipv4.ip_forward'/s/^#//g' /etc/sysctl.conf 
