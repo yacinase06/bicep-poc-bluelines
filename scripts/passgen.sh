bbd4a8ca7b5dcb5491899831088d7119f3c5721d
Shaun Croucher <shauncroucher@gmail.com>
2021-08-26 10:29:45 +0100

    remove unneeded variable in passgen.sh

--- scripts/passgen.sh
+++ scripts/passgen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 len=$1
 secretVal=$(</dev/urandom tr -dc 'a-zA-Z0-9/#@!~$%^&*' | fold -w $len | awk '{c=0} /[a-z]/{c++} /[A-Z]/{c++} /[0-9]/{c++} /[#@!~$%^&*]/{c++} {if (c >= 4){print;exit}}')
-json="{\"SecretVal\":\"$secretName$secretVal\"}"
+json="{\"SecretVal\":\"$secretVal\"}"
 echo "$json" > $AZ_SCRIPTS_OUTPUT_PATH
